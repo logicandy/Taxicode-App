d8fb23bc608ecc8c2b3613a43c5fd6cd33d1bd9d
Josh Thompson <josh3276@gmail.com>
2014-06-19 15:17:53 +0100

    Updated app to be able to handle not having a database class slightly better to test IE.

--- www/js/db.class.js
+++ www/js/db.class.js
@@ -1,66 +1,116 @@
-var DBMC = {
-	
-	db: window.openDatabase(Config.app, 0.1, Config.title+" Database", 1024*1024*2),
+if (!window || typeof window.openDatabase != "function") {
 
-	createTable: function(name, cols, overwrite, success, error) {
-		DBMC.db.transaction(function(tx) {
-			if (overwrite) {
-				tx.executeSql('DROP TABLE IF EXISTS `'+name+'`');
+	var DBMC = {
+		db: null,
+		createTable: function(name, cols, overwrite, success, error) {
+			if (typeof error == "function") {
+				error();
 			}
-			tx.executeSql('CREATE TABLE IF NOT EXISTS '+name+' ('+cols+')');
-		}, error, success);
-	},
+		},
+		deleteTable: function(name, success, error) {
+			if (typeof error == "function") {
+				error();
+			}
+		},
+		insert: function(table, rows, success, error) {
+			if (typeof error == "function") {
+				error();
+			}
+		},
+		select: function(table, selector, where, success, error) {
+			if (typeof error == "function") {
+				error();
+			}
+		},
+		query: function(query, data, success, error) {
+			if (typeof error == "function") {
+				error();
+			}
+		},
+		update: function(success, error) {
+			if (typeof error == "function") {
+				error();
+			}
+		},
+		replace: function(success, error) {
+			if (typeof error == "function") {
+				error();
+			}
+		},
+		delete: function(success, error) {
+			if (typeof error == "function") {
+				error();
+			}
+		}
+	};
 
-	deleteTable: function(name, success, error) {
-		DBMC.db.transaction(function(tx) {
-			tx.executeSql('DROP TABLE IF EXISTS '+name);
-		}, error, success);
-	},
+} else {
 
-	insert: function(table, rows, success, error) {
-		DBMC.db.transaction(function(tx) {
-			$.each(rows, function(i, row) {
-				var keys = '';
-				var escapes = '';
-				var values = [];
-				$.each(row, function(key, value) {
-					keys += (keys==''?'':', ') + '`'+key+'`';
-					escapes += escapes==''?'?':', ?';
-					values.push(value);
-				});
-				tx.executeSql('INSERT INTO `'+table+'` ('+keys+') VALUES ('+escapes+')', values);
-			});
-		}, error, success);
-	},
+	var DBMC = {
+		
+		db: window.openDatabase(Config.app, 0.1, Config.title+" Database", 1024*1024*2),
 
-	select: function(table, selector, where, success, error) {
-		DBMC.db.transaction(function(tx) {
-			tx.executeSql('SELECT '+selector+' FROM `'+table+'` WHERE '+(where?where:1), [], function(tx, results) {
-				var data = [];
-				for (var i = 0; i < results.rows.length; i++) {
-					data.push(results.rows.item(i));
+		createTable: function(name, cols, overwrite, success, error) {
+			DBMC.db.transaction(function(tx) {
+				if (overwrite) {
+					tx.executeSql('DROP TABLE IF EXISTS `'+name+'`');
 				}
-				success(data);
-			}, null, error);
-		}, error);
-	},
+				tx.executeSql('CREATE TABLE IF NOT EXISTS '+name+' ('+cols+')');
+			}, error, success);
+		},
 
-	query: function(query, data, success, error) {
-		DBMC.db.transaction(function(tx) {
-			tx.executeSql(query, data, success, error);
-		});
-	},
+		deleteTable: function(name, success, error) {
+			DBMC.db.transaction(function(tx) {
+				tx.executeSql('DROP TABLE IF EXISTS '+name);
+			}, error, success);
+		},
 
-	update: function(success, error) {
-		alert('DBMC.update() support is coming soon. You can currently use DBMC.db to access the database directly.');
-	},
+		insert: function(table, rows, success, error) {
+			DBMC.db.transaction(function(tx) {
+				$.each(rows, function(i, row) {
+					var keys = '';
+					var escapes = '';
+					var values = [];
+					$.each(row, function(key, value) {
+						keys += (keys==''?'':', ') + '`'+key+'`';
+						escapes += escapes==''?'?':', ?';
+						values.push(value);
+					});
+					tx.executeSql('INSERT INTO `'+table+'` ('+keys+') VALUES ('+escapes+')', values);
+				});
+			}, error, success);
+		},
 
-	replace: function(success, error) {
-		alert('DBMC.replace() support is coming soon. You can currently use DBMC.db to access the database directly.');
-	},
+		select: function(table, selector, where, success, error) {
+			DBMC.db.transaction(function(tx) {
+				tx.executeSql('SELECT '+selector+' FROM `'+table+'` WHERE '+(where?where:1), [], function(tx, results) {
+					var data = [];
+					for (var i = 0; i < results.rows.length; i++) {
+						data.push(results.rows.item(i));
+					}
+					success(data);
+				}, null, error);
+			}, error);
+		},
 
-	delete: function(success, error) {
-		alert('DBMC.delete() support is coming soon. You can currently use DBMC.db to access the database directly.');
-	}
+		query: function(query, data, success, error) {
+			DBMC.db.transaction(function(tx) {
+				tx.executeSql(query, data, success, error);
+			});
+		},
 
-};
+		update: function(success, error) {
+			alert('DBMC.update() support is coming soon. You can currently use DBMC.db to access the database directly.');
+		},
+
+		replace: function(success, error) {
+			alert('DBMC.replace() support is coming soon. You can currently use DBMC.db to access the database directly.');
+		},
+
+		delete: function(success, error) {
+			alert('DBMC.delete() support is coming soon. You can currently use DBMC.db to access the database directly.');
+		}
+
+	};
+
+}
